188c3169bbb95f1416b89a1d4dcd08a11d70a343
Mahdi Malverdi <Mahdi.Malverdi@gmail.com>
2020-04-13 18:36:26 +0430

    Add project files.

---
+++ ThesisProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThesisProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
